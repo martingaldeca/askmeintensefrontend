e9493d48944103997b33df7f66a38795b9c48621
martingaldeca <martingaldeca@gmail.com>
2025-03-31 22:47:30 +0200

    * Add some events

--- src/components/GeneralBottomNavigation/GeneralBottomNavigation.tsx
+++ src/components/GeneralBottomNavigation/GeneralBottomNavigation.tsx
@@ -12,19 +12,20 @@ import { GeneralBottomNavigationStyled } from '@/components/GeneralBottomNavigat
 import { usePathname } from 'next/navigation';
 import { PATHS, PATHS_WITHOUT_NAVBAR } from '@/constants/paths';
 import { useSession } from '@/providers/SessionProvider';
+import { DataService } from "@/app/lib/client";
 
 const GeneralBottomNavigation = () => {
   const [value, setValue] = React.useState(0);
   const router = useRouter();
   const pathname = usePathname();
   const { user } = useSession();
-
+  
   const shouldHideNav = PATHS_WITHOUT_NAVBAR.includes(pathname as PATHS);
-
+  
   if (shouldHideNav) {
     return <></>;
   }
-
+  
   const loginOrProfileText = user?.access ? 'Perfil' : 'Login';
   const loginOrProfileIcon = user?.access ? <PersonIcon /> : <LoginIcon />;
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
@@ -32,6 +33,11 @@ const GeneralBottomNavigation = () => {
     switch (newValue) {
       case 0:
         router.push(PATHS.HOME);
+        DataService.dataEventCreate({
+          event_type: 'navigator_home_click',
+        }).catch(error => {
+          console.error('Error sending page change event:', error);
+        });
         break;
       case 1:
         if (user?.access) {
@@ -41,19 +47,34 @@ const GeneralBottomNavigation = () => {
             toastId: 'login-required',
           });
         }
+        DataService.dataEventCreate({
+          event_type: 'navigator_favorites_click',
+        }).catch(error => {
+          console.error('Error sending page change event:', error);
+        });
         break;
       case 2:
         if (user?.access) {
           router.push(PATHS.PROFILE);
+          DataService.dataEventCreate({
+            event_type: 'navigator_profile_click',
+          }).catch(error => {
+            console.error('Error sending page change event:', error);
+          });
         } else {
           router.push(PATHS.LOGIN_OR_REGISTER);
+          DataService.dataEventCreate({
+            event_type: 'navigator_login_click',
+          }).catch(error => {
+            console.error('Error sending page change event:', error);
+          });
         }
         break;
       default:
         break;
     }
   };
-
+  
   return (
     <GeneralBottomNavigationStyled>
       <BottomNavigation showLabels value={value} onChange={handleChange}>
